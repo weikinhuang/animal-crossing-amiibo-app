5d1f081b598d00f18afd3f1dbd83985d25418d6e
Wei Kin Huang <weikin.huang04@gmail.com>
2015-10-11 17:57:31 -0400

    switch to sidemenu app

--- www/app/components/frame/frame.module.js
+++ www/app/components/frame/frame.module.js
@@ -1,13 +1,14 @@
 import angular from "angular";
 
 import FrameViewCtrl from "./frame-view.controller";
-import acFrameView from "./frame-view.directive";
 
 export const module = angular.module("app.frame", []);
 export default module.name;
 
+export const routeTemplate = "app/components/frame/frame.html";
+export const routeController = "FrameViewCtrl";
+
 module
 	.controller("FrameViewCtrl", FrameViewCtrl)
-	.directive("acFrameView", acFrameView)
 ;
 
--- www/app/routes.js
+++ www/app/routes.js
@@ -1,3 +1,5 @@
+import { routeTemplate as frameRouteTemplate, routeController as frameRouteController } from "./components/frame/frame.module";
+
 function routerConfig($stateProvider, $urlRouterProvider) {
 	// Ionic uses AngularUI Router which uses the concept of states
 	// Learn more here: https://github.com/angular-ui/ui-router
@@ -6,51 +8,53 @@ function routerConfig($stateProvider, $urlRouterProvider) {
 	$stateProvider
 
 		// setup an abstract state for the tabs directive
-		.state("tab", {
-			url : "/tab",
+		.state("app", {
+			url : "/app",
 			abstract : true,
-			template : "<ac-frame-view></ac-frame-view>"
+			templateUrl : frameRouteTemplate,
+			controller : `${frameRouteController} as viewCtrl`
 		})
 
 		// Each tab has its own nav history stack:
 
-		.state("tab.dash", {
+		.state("app.dash", {
 			url : "/dash",
 			views : {
-				"tab-dash" : {
-					template : "<ac-dash-view></ac-dash-view>"
+				content : {
+					templateUrl : "app/components/dash/tab-dash.html"
+					// template : `<ac-dash-view style="translate3d(0%, 0px, 0px)"></ac-dash-view>`
 				}
 			}
 		})
 
-		.state("tab.chats", {
+		.state("app.chats", {
 			url : "/chats",
 			views : {
-				"tab-chats" : {
+				content : {
 					template : "<ac-chats-view></ac-chats-view>"
 				}
 			}
 		})
-		.state("tab.chat-detail", {
+		.state("app.chat-detail", {
 			url : "/chats/:chatId",
 			views : {
-				"tab-chats" : {
+				content : {
 					template : "<ac-chat-detail-view></ac-chat-detail-view>"
 				}
 			}
 		})
 
-		.state("tab.account", {
+		.state("app.account", {
 			url : "/account",
 			views : {
-				"tab-account" : {
+				content : {
 					template : "<ac-account-view></ac-account-view>"
 				}
 			}
 		});
 
 	// if none of the above states are matched, use this as the fallback
-	$urlRouterProvider.otherwise("/tab/dash");
+	$urlRouterProvider.otherwise("/app/dash");
 }
 
 export default [
