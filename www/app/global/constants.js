e175c9444b53539ce555cca04a661100cc9bb53a
Wei Kin Huang <weikin.huang04@gmail.com>
2016-01-31 21:22:34 -0500

    Update app version number

--- www/app/global/constants.js
+++ www/app/global/constants.js
@@ -7,7 +7,7 @@ export default module.name;
 
 
 module
-	.constant("APP_VERSION", "1.0.0")
+	.constant("APP_VERSION", "1.0.2")
 	.constant("LATEST_SERIES", 2)
 	.constant("MIN_CARD_ID", 1)
 	.constant("MAX_CARD_ID", 200)
