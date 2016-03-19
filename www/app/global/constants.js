49b399b585ec08fa2274bfd00b9dc40c31839da8
Wei Kin Huang <weikin.huang04@gmail.com>
2016-03-18 22:46:22 -0400

    Add wave 3 cards

--- www/app/global/constants.js
+++ www/app/global/constants.js
@@ -7,8 +7,8 @@ export default module.name;
 
 
 module
-	.constant("APP_VERSION", "1.0.2")
-	.constant("LATEST_SERIES", 2)
+	.constant("APP_VERSION", "1.0.3")
+	.constant("LATEST_SERIES", 3)
 	.constant("MIN_CARD_ID", 1)
-	.constant("MAX_CARD_ID", 200)
+	.constant("MAX_CARD_ID", 300)
 ;
